46238e368de3e9e5939f3ee21a9471ec4b34d103
Franziska Schneider <franzisak.schneider@me.com>
2019-04-22 00:44:47 +0200

    change look of footer

--- src/app/custom-footer/custom-footer.component.ts
+++ src/app/custom-footer/custom-footer.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-custom-footer',
   templateUrl: './custom-footer.component.html',
-  styleUrls: ['./custom-footer.component.css']
+  styleUrls: ['./custom-footer.component.scss']
 })
 export class CustomFooterComponent implements OnInit {
 

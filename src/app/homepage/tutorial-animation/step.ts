3cc64634c7dbfbf31641b4a870b441b932baa67f
Franziska Schneider <franziska.schneider@me.com>
2019-05-19 22:43:25 +0200

    bla ich weiß auCH NICHT so genau

---
+++ src/app/homepage/tutorial-animation/step.ts
@@ -0,0 +1,7 @@
+import { TemplateRef } from "@angular/core";
+
+export class Step {
+    step: TemplateRef<any>;
+    name: any;
+    constructor() {}
+}
--- src/app/homepage/tutorial-animation/tutorial-animation.component.ts
+++ src/app/homepage/tutorial-animation/tutorial-animation.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit, NgZone, ViewChild, ViewContainerRef, TemplateRef } from '@angular/core';
+import { Component, OnInit, NgZone, ViewChild, ViewContainerRef, TemplateRef, ViewRef } from '@angular/core';
 import { Animations } from './animations';
 import { ScrollDispatcher } from '@angular/cdk/overlay';
 import { Subscription } from 'rxjs';
-import { VizWaves04Component } from 'src/app/widget-content/waves04/viz-waves04.component';
+import { _MatChipListMixinBase } from '@angular/material';
+import { Step } from './step';
+import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-tutorial-animation',
@@ -16,45 +18,56 @@ import { VizWaves04Component } from 'src/app/widget-content/waves04/viz-waves04.
 export class TutorialAnimationComponent implements OnInit {
 
     @ViewChild('entry', {read: ViewContainerRef}) entry: ViewContainerRef;
-    @ViewChild('step-01') step01: TemplateRef<any>;
-    @ViewChild('step-02') step02: TemplateRef<any>;
+    @ViewChild('step01') step01: TemplateRef<any>;
+    @ViewChild('step02') step02: TemplateRef<any>;
+    @ViewChild('step03') step03: TemplateRef<any>;
+    @ViewChild('step04') step04: TemplateRef<any>;
+    @ViewChild('step05') step05: TemplateRef<any>;
+    @ViewChild('step06') step06: TemplateRef<any>;
+
+    private faAngleDown = faAngleDown;
     private isBlack = false;
     private _tutorialStep: number;
-    private steps = [
-        'What is a cell?',
-        'What is an automaton?',
-        'Ruleset',
-        'Possible Rulesets',
-        'Array Modes'
-    ]
+    private _currentTotalSteps = 0;
+    private steps: Step[];
     private multipleCells = [];
     private _scrollSubscription: Subscription;
+    
     constructor(
                 private scrollDispatcher: ScrollDispatcher,
                 private zone: NgZone) {
                     this._tutorialStep = 0;
-                 }
+                }
 
         set tutorialStep(step: number) {
             this._tutorialStep = step;
             console.log(step);
         }
 
-                ngOnInit() {
-      this._scrollSubscription = this.scrollDispatcher.scrolled(100).subscribe(
+    ngOnInit() {
+        this.steps = [
+            {step: this.step01, name: 'What is a cell?'},
+            {step: this.step02, name: 'What is an automaton?'},
+            {step: this.step03, name: 'What is a cell?'},
+            {step: this.step04, name: 'What is an automaton?'},
+            {step: this.step05, name: 'What is a cell?'},
+            {step: this.step06, name: 'What is an automaton?'}
+        ];
+        this._scrollSubscription = this.scrollDispatcher.scrolled(100).subscribe(
           () => {
             console.log("scrolled");
             this.zone.run<void>(
                 () => {
                     this.addTwoSmallCellsToAnimation();
                 });
-          });
-  }
-  triggerChange() {
-      this.isBlack = !this.isBlack;
-  }
+        });
+    }
 
-  addTwoSmallCellsToAnimation() {
+    triggerChange() {
+        this.isBlack = !this.isBlack;
+    }
+
+    addTwoSmallCellsToAnimation() {
         if (this.multipleCells.length < 17) {
             this.multipleCells.push(Math.round(Math.random()));
             this.multipleCells.unshift(Math.round(Math.random()));
@@ -63,7 +76,14 @@ export class TutorialAnimationComponent implements OnInit {
         }
     }
 
-    addStep() {
-        return;
+    addStep(stepId) {
+        const stepEntry = this.steps[stepId];
+        if (this._currentTotalSteps >= this.steps.length) {
+            console.log('already existent');
+            return;
+        }
+        const stepToAdd = stepEntry.step.createEmbeddedView(this);
+        this.entry.insert(stepToAdd);
+        this._currentTotalSteps++;
     }
 }

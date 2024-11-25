80bcf0941abdb3f61aec87f605a6a7b80e587e5a
Franziska Schneider <franz@Freund-Rechner.local>
2024-11-25 20:06:28 +0100

    Simplifies Automaton controller component

--- src/app/controls-ui/topbar/topbar.component.ts
+++ src/app/controls-ui/topbar/topbar.component.ts
@@ -1,10 +1,9 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { AutomatonService } from 'src/app/services/automaton.service';
-import { faPlay, faUndo, faAngleLeft, faPause, faHome } from '@fortawesome/free-solid-svg-icons';
+import { faPlay, faUndo, faAngleLeft, faPause } from '@fortawesome/free-solid-svg-icons';
 import { MatButton } from '@angular/material/button';
 import { MatTooltip } from '@angular/material/tooltip';
 import { FaIconComponent } from '@fortawesome/angular-fontawesome';
-import { StatusDisplayComponent } from '../status-display/status-display.component';
 import { MatDivider } from '@angular/material/divider';
 import { AutomatonControllerComponent } from '../automaton-controller/automaton-controller.component';
 
@@ -13,7 +12,7 @@ import { AutomatonControllerComponent } from '../automaton-controller/automaton-
     templateUrl: './topbar.component.html',
     styleUrls: ['./topbar.component.scss'],
     standalone: true,
-    imports: [MatButton, MatTooltip, FaIconComponent, StatusDisplayComponent, MatDivider, AutomatonControllerComponent]
+    imports: [MatButton, MatTooltip, FaIconComponent, MatDivider, AutomatonControllerComponent]
 })
 export class TopbarComponent implements OnInit 
 {
@@ -21,27 +20,31 @@ export class TopbarComponent implements OnInit
     readonly faPause = faPause;
     readonly faUndo = faUndo;
     readonly faAngleLeft = faAngleLeft;
-    private _controllerDisplayed: boolean;
-    private _isRunning = false;
+    controllerDisplayed: boolean;
+    isRunning = false;
+    generation: number;
 
     constructor(private automaton: AutomatonService) { }
 
     ngOnInit() 
     {
-        this._controllerDisplayed = window.innerWidth > 420 ? true : false;
+        this.controllerDisplayed = window.innerWidth > 420 ? true : false;
         this.init = this.ngOnInit.bind(this);
+        this.update = this.update.bind(this);
         this.automaton.ready$.subscribe(this.init)
+        this.automaton.changed$.subscribe(this.update)
+        this.automaton.cellsChanged$.subscribe(this.update);
     }
 
     init(): void
     {
-        this._isRunning = this.automaton.isRunning;
+        this.isRunning = this.automaton.isRunning;
     }
 
     onClickPlay()
     {
-        this._isRunning = !this._isRunning;
-        this.automaton.isRunning = this._isRunning;
+        this.isRunning = !this.isRunning;
+        this.automaton.isRunning = this.isRunning;
     }
 
     onClickReset()
@@ -51,10 +54,16 @@ export class TopbarComponent implements OnInit
 
     toggleController()
     {
-        this._controllerDisplayed = !this._controllerDisplayed;
+        this.controllerDisplayed = !this.controllerDisplayed;
     }
 
-    hideController() {
-        this._controllerDisplayed = false;
+    hideController() 
+    {
+        this.controllerDisplayed = false;
+    }
+
+    update()
+    {
+        this.generation = this.automaton.generation;
     }
 }

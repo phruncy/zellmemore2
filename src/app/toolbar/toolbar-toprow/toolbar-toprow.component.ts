42a029c0fdb269621117db5112c152d5ac396faa
Franziska Schneider <franz@Freund-Rechner.local>
2024-12-11 19:45:50 +0100

    Bugfix: Fixes broken router Link in ProgramWindow Component

--- src/app/toolbar/toolbar-toprow/toolbar-toprow.component.ts
+++ src/app/toolbar/toolbar-toprow/toolbar-toprow.component.ts
@@ -4,13 +4,14 @@ import { faPlay, faUndo, faAngleLeft, faPause, faHome } from '@fortawesome/free-
 import { MatButton } from '@angular/material/button';
 import { MatTooltip } from '@angular/material/tooltip';
 import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-toolbar-toprow',
     templateUrl: './toolbar-toprow.component.html',
     styleUrls: ['./toolbar-toprow.component.scss'],
     standalone: true,
-    imports: [MatButton, MatTooltip, FaIconComponent]
+    imports: [MatButton, MatTooltip, FaIconComponent, RouterLink]
 })
 export class ToolbarToprowComponent implements OnInit 
 {

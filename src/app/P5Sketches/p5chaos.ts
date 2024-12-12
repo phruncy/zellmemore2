b9dd7e5c9ade78335d9c72384bc0623195fedafd
Franziska Schneider <franziska.schneider@me.com>
2024-12-12 13:47:48 +0100

    Adds 'chaos' visualization as P5 sketch

--- src/app/P5Sketches/p5chaos.ts
+++ src/app/P5Sketches/p5chaos.ts
@@ -6,6 +6,78 @@ export const p5chaos = new P5Sketch
     "chaos",
     function chaosSketch(p5: p5): void 
     {
+        let blockSize: number;
+        let maxDisplayableGenerations: number;
+        let buffer: ImageData;
+        
+        const initValues = () => 
+        {
+            blockSize = p5.width / this.automaton.cellnumber;
+            maxDisplayableGenerations = Math.floor(p5.height / blockSize);
+        }
 
+        const getPositionY = (): number => Math.min(this.automaton.generation, maxDisplayableGenerations - 1) * blockSize;
+        
+        const drawCurrentGeneration = () =>
+        {
+            const random: number[] = this.automaton.states.sort((a, b) => 0.5 - Math.random());
+            const positionY = getPositionY();
+            random.forEach((state, index) => 
+                {
+                    if (state === 1) { p5.square(index * blockSize, positionY, blockSize);}
+                })
+        }
+
+        const addCurrentToFrameBuffer = () =>
+        {
+            drawCurrentGeneration();
+            buffer = p5.drawingContext.getImageData(0, 0, p5.width, p5.height);
+        }
+
+        const initFrameBuffer = () =>
+        {
+            p5.background(255);
+            addCurrentToFrameBuffer();
+        }
+
+        p5.setup = () => 
+        {
+            p5.createCanvas(this.componentWidth, this.componentHeight);
+            p5.background(255);
+            p5.fill(0);
+            p5.noStroke();
+            initValues();
+            initFrameBuffer();
+        }
+
+        p5.draw = () => 
+        {
+            p5.background(255);
+            p5.drawingContext.putImageData(buffer, 0, 0);
+            buffer = p5.drawingContext.getImageData(0, 0, p5.width, p5.height);
+        }
+
+        p5.automatonStateUpdate = () => 
+        {
+            p5.background(255);
+            const anchorY = this.automaton.generation <= maxDisplayableGenerations ? 0 : -blockSize;
+            p5.drawingContext.putImageData(buffer, 0, anchorY);
+            addCurrentToFrameBuffer();
+        }
+        
+        p5.componentResize = () => 
+        {
+            p5.resizeCanvas(this.componentWidth, this.componentHeight);
+            initValues();
+            initFrameBuffer();
+        }
+
+        p5.automatonReset = () => 
+        { 
+            initValues();
+            initFrameBuffer();
+        }
+
+        p5.automatonModeChange = () => {}
     }
 );

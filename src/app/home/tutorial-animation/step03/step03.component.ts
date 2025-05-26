97f894e554d17866bd7893d0dedd041723167500
Franziska Schneider <franz@Freund-Rechner.local>
2025-05-26 20:54:58 +0200

    Inline Step 3 template

--- src/app/home/tutorial-animation/step03/step03.component.ts
+++ src/app/home/tutorial-animation/step03/step03.component.ts
@@ -2,9 +2,46 @@ import { Component, input } from '@angular/core';
 
 @Component({
     selector: 'app-step03',
-    templateUrl: './step03.component.html',
     styleUrls: ['./step03.component.scss'],
     standalone: true,
+    template: `<div class="graphics-container">
+        <div class="cell-row">
+            <div class="cell-container">
+                <div class="signal left" [class.animate-step02]="activeDescription() >= 2"></div>
+                <div
+                    class="background-circle outer"
+                    [class.animate-step01]="activeDescription() === 1"
+                    [class.animate-step02]="activeDescription() >= 2"></div>
+                <div class="cell black" id="cell1"></div>
+            </div>
+            <div class="cell-container">
+                <div
+                    class="connector left"
+                    [class.animate-step01]="activeDescription() === 1"
+                    [class.animate-step02]="activeDescription() >= 2"></div>
+                <div
+                    class="connector right"
+                    [class.animate-step01]="activeDescription() === 1"
+                    [class.animate-step02]="activeDescription() >= 2"></div>
+                <div
+                    class="background-circle"
+                    [class.animate-step01]="activeDescription() === 1"
+                    [class.animate-step02]="activeDescription() >= 2"></div>
+                <div
+                    class="cell"
+                    id="cell2"
+                    [class.animate-step02]="activeDescription() >= 2"></div>
+            </div>
+            <div class="cell-container">
+                <div class="signal right" [class.animate-step02]="activeDescription() >= 2"></div>
+                <div
+                    class="background-circle outer"
+                    [class.animate-step01]="activeDescription() === 1"
+                    [class.animate-step02]="activeDescription() >= 2"></div>
+                <div class="cell" id="cell3"></div>
+            </div>
+        </div>
+    </div>`,
 })
 export class Step03Component {
     activeDescription = input.required<number>();
